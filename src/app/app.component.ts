1cc80e390a2a3b7351159b5da9fc3a634be7de1f
fusho-takahashi <zenryaku@fusho-takahashi.me>
2020-06-16 00:09:06 +0900

    ルーレット実装

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  talkThemes: string[] = [
+    '好きなおにぎりの具',
+    '好きな女性のタイプ',
+    '今日のお昼ごはん',
+  ];
+  selectedTheme: string;
+  rouletteTime = false;
+
+  ngOnInit() {
+    const randomNumber = Math.floor(Math.random() * 3);
+    this.selectedTheme = this.talkThemes[randomNumber];
+  }
+
+  rouletteStart() {
+    this.rouletteTime = true;
+    let counter = 0;
+    const roulette = setInterval(() => {
+      this.selectedTheme = this.talkThemes[counter];
+      counter = counter < this.talkThemes.length - 1 ? counter + 1 : 0;
+      if (!this.rouletteTime) {
+        clearInterval(roulette);
+      }
+    }, 100);
+  }
+
+  rouletteStop() {
+    this.rouletteTime = false;
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { FormsModule } from '@angular/forms';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
