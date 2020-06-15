5e93d91edd808276e89171e032fe2ff2c8545669
fusho-takahashi <zenryaku@fusho-takahashi.me>
2020-06-15 23:13:09 +0900

    ng add @angular/material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'talk-theme-roulette';
-}
+export class AppComponent {}
